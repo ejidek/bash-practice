2abedd1482d797b4a9ac09171a0c125cecee20eb
Abdulhamid <ejiwumiadekunle@gmail.com>
2025-11-13 11:00:02 +0100

    Third commit to local repo

---
+++ checkfile.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# =======================================================
+# name: Adekunle
+# date: 2025
+# version: 1.0.0
+#
+# about: This script will be use to check if file exist
+# =======================================================
+
+if [ -f "file.txt" ]; then
+	echo "file exist"
+else 
+	echo "file does not exist"
+fi
+
+
---
+++ practice.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Name: Adekunle
+# About: to take input then print it back to the console
+
+
+read -p "What is your age?" age
+echo "I am $age years old now";
