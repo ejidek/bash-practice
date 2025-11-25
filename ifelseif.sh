107b2b14872757a12a2892ac308c37ce7c98936c
Abdulhamid <ejiwumiadekunle@gmail.com>
2025-11-25 14:22:37 +0100

    Modified the ifelseif.sh file

--- ifelseif.sh
+++ ifelseif.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+# this condition is to check for username
 if [ ${1,,} = adekunle ]; then
+	# use this line to print welcome message
 	echo "Welcome back boss you are welcome to the office"
+	# this second condition is to check for help argument
 elif [ ${1,,} = help ]; then
+	# print out help message
 	echo "Just type in the UserName"
 else 
+	# print out error message
 	echo "I dont know you Mr Man, Fashi jhoor"
 fi
 
